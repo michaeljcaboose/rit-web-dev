6cdc1eff399dab3dd27a6274e384cf508657eff8
Michael J Caboose <mj.panebianco@gmail.com>
2017-11-16 01:16:27 -0500

    added links to gear page

--- 645/project2/gear.php
+++ 645/project2/gear.php
@@ -16,6 +16,19 @@
             This webpage will explain the history of the position of Goaltender, the gear required,
             and insights into how to play the position effectively.
         </p>
+        <p>
+            <a name="gear-menu"></a>
+            <ul>
+                <li><a href="#mask">Masks</a></li>
+                <li><a href="#skate">Skates</a></li>
+                <li><a href="#legpad">Leg Pads</a></li>
+                <li><a href="#chest">Chest Protection</a></li>
+                <li><a href="#catcher">Catcher</a></li>
+                <li><a href="#blocker">Blocker</a></li>
+                <li><a href="#pants">Pants</a></li>
+                <li><a href="#stick">Stick</a></li>
+            </ul>
+        </p>
     </div>
 </div>
 </body>
