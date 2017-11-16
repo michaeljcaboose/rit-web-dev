57a969a79e59519e31cc52cef1d5229354ff6ee1
Matthew Panebianco <mpane1@u.brockport.edu>
2017-11-16 12:15:26 -0500

    history page complete

--- 645/project2/history.php
+++ 645/project2/history.php
@@ -13,8 +13,73 @@
     </div>
     <div class="div_copy">
         <p>
-            This webpage will explain the history of the position of Goaltender, the gear required,
-            and insights into how to play the position effectively.
+            While Hockey and similar sports have been played informally since the 16th century, the first organized
+            version of what would become modern Ice Hockey began in Montreal, Canada in the 1870s. The position of
+            "Goaltender" would be formalized during this time. Goaltenders were essentially an extra player on the ice,
+            with the only extra padding being a glove and a precursor to modern leg pads. Less padding was used because
+            players used flat faced wooden sticks, which reduced the speed and elevation of shots. The slapshot also had
+            not become popular yet.
+        </p>
+        <figure>
+            <img src="media/1905_stanley_cup.jpg">
+            <figcaption>1905 Ottawa Senators featuring their goaltender in far bottom-left</figcaption>
+        </figure>
+
+        <p>
+            The position wouldn't change much until the 1960s as the snapshot became more popular and players were shooting
+            faster and higher than ever before. This is when masks started to appear on the faces of goaltenders around
+            the league, and the last goaltender to play in the National Hockey League without a mask played in 1974. All
+            padding around the goaltender had been increased, from the legs up to the chest. Thicker gloves were being
+            used for catchers, blockers began to morph into their modern "waffle-board" shape, and chest protection began
+            to cover the entire upper body rather than only the chest.
+        </p>
+
+        <p>The position wouldn't change much until the 1960s as the snapshot became more popular and players were shooting
+            faster and higher than ever before. This is when masks started to appear on the faces of goaltenders around
+            the league, and the last goaltender to play in the National Hockey League without a mask played in 1974. All
+            padding around the goaltender had been increased, from the legs up to the chest. Thicker gloves were being
+            used for catchers, blockers began to morph into their modern "waffle-board" shape, and chest protection began
+            to cover the entire upper body rather than only the chest.
+        </p>
+        <p>
+            As hockey entered the 1980s, players started to shoot higher and faster shots on goaltenders. For decades,
+            goaltenders had utilized a style of play called "Stand-up" where they would remain on their feet to make a
+            majority of their saves, usually with their glove hand or by kicking out their legs. This was because most
+            shots were low and fast. Hall of Fame players such as Jacque Plante were masters of this style of play during
+            the time.
+        </p>
+        <figure>
+            <img src="media/standup_large.jpg">
+            <figcaption>A stand-up goaltender deep in his net</figcaption>
+        </figure>
+
+        <p>
+            As the game of hockey changed for skaters, goaltenders needed to adjust to these new shot patterns. The 80s
+            and 90s saw goalies transition from "Stand-up" to a style called "Butterfly" where goalies would drop to their
+            knees and flair their legs out to cover the bottom of the net. The pioneer of this style in the late 80s and 90s
+            was Patrick Roy, who is considered one of the greatest goaltenders of all time.
+        </p>
+        <figure>
+            <img src="media/roy.jpg">
+            <figcaption>Hall of Famer Patrick Roy in his "butterfly" stance making an upper body save</figcaption>
+        </figure>
+
+        <p>
+            As this style became more popular, the gear started changing to fit the style, and eventually players where
+            put into two groups: "Butterfly" and "Hybrid", with hybrid being a mixture of the "Stand-up" and "Butterfly"
+            styles. All-time great goaltenders such as Dominique Hasek and Martin Brodeur were known to play more of a hybrid
+            style, utilizing butterfly techniques while still being flexible in their save selection styles.
+        </p>
+        <figure>
+            <img src="media/dominik-hasek.jpg">
+            <figcaption>Hall of Famer Dominik Hasek making an unorthodox save</figcaption>
+        </figure>
+
+        <p>
+            Today, with the speed of the game and the talent and abilities of the skaters shooting on them, goaltenders
+            mix and match techniques while being more mobile and athletic than their predecessors. Most goaltenders are
+            considered to be hybrid, stopping the pucks any way they can rather than sticking to one concrete style,
+            though some goaltenders are notably "butterfly"-only goalies.
         </p>
     </div>
 </div>
